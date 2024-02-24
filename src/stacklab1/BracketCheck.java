08a0ea9864ddfa2ce1933d982c81c72de7dab6f8
Thisara <sasmithajayasinghe1@gmail.com>
2024-02-24 16:24:18 +0530

    co

--- src/stacklab1/BracketCheck.java
+++ src/stacklab1/BracketCheck.java
@@ -1,20 +1,22 @@
 package stacklab1;
 
 public class BracketCheck {
+    //Create variable
     private String inputString;
 
     public BracketCheck(String str){
         inputString=str;
     }
     public void check(){
+        //assign size to maxSize
         int size = inputString.length();
         Stack stObj= new Stack(size);
 
         for(int i =0;i<size;i++){
-            if(inputString.charAt(i)=='('){
+            if(inputString.charAt(i)=='('){  //if ( push to stack
                 stObj.push('(');
-            } else if (inputString.charAt(i)==')') {
-                stObj.pop();
+            } else if (inputString.charAt(i)==')') { //if ')' pop one pushed element from stack
+                stObj.pop();                        //so pop all pushed elements if brackets equal
             }
         }
         if(!stObj.isEmpty()){
